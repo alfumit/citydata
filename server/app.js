4f153456fdafb0d5459f729512fcf6f21115fb37
Alex Fedotov <alexander.g.fedotov@kaspersky.com>
2017-02-19 22:25:10 +0300

    Trying to launch the server

--- server/app.js
+++ server/app.js
@@ -4,12 +4,13 @@ var express = require("express"),
     app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../front-end/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-console.log(path.join(__dirname, '../front-end/index.html'))
-//app.use(express.static(path.join(__dirname, '../front-end/index.html')));
+//console.log(path.join(__dirname, '../front-end/index.html'))
+//app.use(express.static(path.join(__dirname, '/index.html')));
 
 app.listen(3000, function() {
+    console.log(path.join(__dirname, '/index.html'))
     console.log('Example app listening on port 3000!');
 });
