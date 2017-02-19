7497d126c2ce1307905fb8bb367fc4c0f685a2a6
Alex Fedotov <alexander.g.fedotov@kaspersky.com>
2017-02-19 21:51:01 +0300

    More test

--- server/app.js
+++ server/app.js
@@ -2,9 +2,13 @@
 var express = require("express"),
     path = require("path"),
     app = express();
+
 app.get('/', function(req, res) {
-    res.sendFile('/../front-end/index.html');
+    res.sendFile(path.join(__dirname, '../front-end/index.html'));
 });
+
+console.log(path.join(__dirname, '../front-end/index.html'))
+//app.use(express.static(path.join(__dirname, '../front-end/index.html')));
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
