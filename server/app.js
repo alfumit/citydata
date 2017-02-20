60d7f35c29bdd6ed0effeb0ba73003e5618b9910
Alexander Fedotov <alexander.g.fedotov@kaspersky.com>
2017-02-20 12:03:17 +0300

    Further work

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ var express = require("express"),
     path = require("path"),
     app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res) {
     res.send("Hello world");
 });
@@ -10,7 +12,6 @@ app.get('/', function(req, res) {
 //console.log(path.join(__dirname, '../front-end/index.html'))
 //app.use(express.static(path.join(__dirname, '/index.html')));
 
-app.listen(3000, function() {
-    console.log(path.join(__dirname, '/index.html'))
-    console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
