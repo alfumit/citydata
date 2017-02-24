d6688fd9719c33ed66828472edebe124f5630bfd
Alex Fedotov <alexander.g.fedotov@kaspersky.com>
2017-02-24 18:28:11 +0300

    More PG testing

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 "use strict";
 var express = require("express"),
     path = require("path"),
-    app = express();
+    app = express(),
+    pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -20,17 +21,16 @@ app.get('/times', function(request, response) {
   response.send(result);
 });
 
-var pg = require('pg');
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
+//    client.query('SELECT * FROM test_table', function(err, result) {
+//      done();
+//      if (err)
+//       { console.error(err); response.send("Error " + err); }
+//      else
+//       { response.render('pages/db', {results: result.rows} ); }
+//    });
   });
 });
 
