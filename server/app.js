19d64c21c8fb4aa891479594fd0e8fb641d2077f
Alex Fedotov <alexander.g.fedotov@kaspersky.com>
2017-02-24 22:46:19 +0300

    Request result test

--- server/app.js
+++ server/app.js
@@ -15,15 +15,14 @@ app.get('/', function(req, res) {
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-        console.log(result);
       response.send("AYAYAYAY ");
+    //client.query('SELECT * FROM test_table', function(err, result) {
 //      done();
 //      if (err)
 //       { console.error(err); response.send("Error " + err); }
 //      else
 //       { response.render('pages/db', {results: result.rows} ); }
-    });
+    //});
   });
 });
 
