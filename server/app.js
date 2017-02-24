5d3734783146b192921139bf5765e849bd9e71c9
Alex Fedotov <alexander.g.fedotov@kaspersky.com>
2017-02-24 21:18:11 +0300

    Created remote db

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.get('/times', function(request, response) {
 
 
 app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+ // pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 //    client.query('SELECT * FROM test_table', function(err, result) {
 //      done();
 //      if (err)
@@ -31,7 +31,7 @@ app.get('/db', function (request, response) {
 //      else
 //       { response.render('pages/db', {results: result.rows} ); }
 //    });
-  });
+//  });
 });
 
 app.listen(app.get('port'), function() {
