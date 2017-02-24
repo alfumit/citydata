1e3428ec0cc3c40c931374f290a1bac020da58da
Alex Fedotov <alexander.g.fedotov@kaspersky.com>
2017-02-24 18:02:13 +0300

    Fixed heroku sample errors

--- server/app.js
+++ server/app.js
@@ -13,8 +13,8 @@ app.get('/', function(req, res) {
 //app.use(express.static(path.join(__dirname, '/index.html')));
 
 app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
+    var result = '', i=0;
+    var times = process.env.TIMES || 5;
     for (i=0; i < times; i++)
       result += i + ' ';
   response.send(result);
