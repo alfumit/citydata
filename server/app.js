d4bbad26e7ead2cdf896d8264624d2dc30afc627
Alex Fedotov <alexander.g.fedotov@kaspersky.com>
2017-02-26 10:48:31 +0300

    One more attempt on db

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.get('/', function(req, res) {
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      response.send("AYAYAYAY ");
+      //response.send("AYAYAYAY ");
       console.log(client);
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
